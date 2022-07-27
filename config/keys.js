4d5bff680c8f24f269d0fe7049d680e4a04732fc
merinpriyasha <68733175+merinpriyasha@users.noreply.github.com>
2022-07-27 15:03:54 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config();
 
 module.exports = {
-    mongoDBUri: 'mongodb+srv://MERN:mern@mernproject.zum2gmm.mongodb.net/test',
+    mongoDBUri: '',
     JWT_SECRET: 'jwtPrivateKey',
     EMAIL_PASS: 'jrrwbmmpwibtsxxl',
     EMAIL_ADDRESS: 'merinpriyasha@gmail.com'
